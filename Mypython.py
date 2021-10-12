77de207d4936d62a56bd86a41eb192aa2f7c1ab8
OliviaFortuneUCD <81123269+OliviaFortuneUCD@users.noreply.github.com>
2021-10-12 18:53:31 +0100

    Create Mypython.py

---
+++ Mypython.py
@@ -0,0 +1,9 @@
+# import genfromtxt function
+from numpy import genfromtxt
+# Read comma separated file
+product_data = genfromtxt('gold.csv', delimiter=',')
+
+# display initial 5 records
+print(product_data)
+# display initial 5 records
+print(product_data)
